99909c9b790acb1a56a7666377dae861ac7071b8
wfnuser <wfnuser@hotmail.com>
2025-06-16 23:12:31 +0800

    fix: update api

--- client/models/import-project-dto.ts
+++ client/models/import-project-dto.ts
@@ -46,7 +46,7 @@ export interface ImportProjectDto {
      * @type {object}
      * @memberof ImportProjectDto
      */
-    'tutorial': object;
+    'tutorial'?: object;
     /**
      * 
      * @type {YoubetExtra}
